5f2360b11b4ca754208d3ef5f54ef9dfa7a9e45c
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-17 03:29:48 +0200

    squash!

--- Source/kernels/api/api.ts
+++ Source/kernels/api/api.ts
@@ -20,16 +20,8 @@ const allowedAccessToProposedApi = new Set(['ms-toolsai.datawrangler', 'donjayam
 
 export function getKernelsApi(extensionId: string): Kernels {
     return {
-        async findKernel(uri: Uri) {
-            const accessAllowed = await requestKernelAccess(extensionId);
-            sendTelemetryEvent(Telemetry.NewJupyterKernelsApiUsage, undefined, {
-                extensionId,
-                pemUsed: 'findKernel',
-                accessAllowed
-            });
-            if (!accessAllowed) {
-                return;
-            }
+        async getKernel(uri: Uri) {
+            let accessAllowed: boolean | undefined = undefined;
 
             const kernelProvider = ServiceContainer.instance.get<IKernelProvider>(IKernelProvider);
             const notebooks = ServiceContainer.instance.get<IVSCodeNotebook>(IVSCodeNotebook);
@@ -37,6 +29,11 @@ export function getKernelsApi(extensionId: string): Kernels {
             const kernel = kernelProvider.get(notebook || uri);
             // We are only interested in returning kernels that have been started by the user.
             if (!kernel || !kernel.startedAtLeastOnce) {
+                sendTelemetryEvent(Telemetry.NewJupyterKernelsApiUsage, undefined, {
+                    extensionId,
+                    pemUsed: 'getKernel',
+                    accessAllowed
+                });
                 return;
             }
             const execution = kernelProvider.getKernelExecution(kernel);
@@ -44,8 +41,24 @@ export function getKernelsApi(extensionId: string): Kernels {
                 // For local kernels, execution count must be greater than 0,
                 // As we pre-warms kernels (i.e. we start kernels even though the user may not have executed any code).
                 // The only way to determine whether users executed code is to look at the execution count
+                sendTelemetryEvent(Telemetry.NewJupyterKernelsApiUsage, undefined, {
+                    extensionId,
+                    pemUsed: 'getKernel',
+                    accessAllowed
+                });
                 return;
             }
+            // Check and prompt for access only if we know we have a kernel.
+            accessAllowed = await requestKernelAccess(extensionId);
+            sendTelemetryEvent(Telemetry.NewJupyterKernelsApiUsage, undefined, {
+                extensionId,
+                pemUsed: 'getKernel',
+                accessAllowed
+            });
+            if (!accessAllowed) {
+                return;
+            }
+
             let wrappedKernel = kernelCache.get(kernel) || createKernelApiForExtension(extensionId, kernel);
             kernelCache.set(kernel, wrappedKernel);
             return wrappedKernel;
