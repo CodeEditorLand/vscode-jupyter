570f47167e876f3c59268d45fc3c35085c0cbdc0
Nikola Hristov <nikola@nikolahristov.tech>
2023-12-04 12:17:15 +0200

    squash!

--- types/ansi-to-html.d.ts
+++ types/ansi-to-html.d.ts
@@ -1,10 +1,10 @@
-declare module 'ansi-to-html' {
-    export = ansiToHtml;
-    class ansiToHtml {
-        constructor(options?: any);
-        opts: any;
-        stack: any;
-        stickyStack: any;
-        toHtml(input: any): any;
-    }
+declare module "ansi-to-html" {
+	export = ansiToHtml;
+	class ansiToHtml {
+		constructor(options?: any);
+		opts: any;
+		stack: any;
+		stickyStack: any;
+		toHtml(input: any): any;
+	}
 }
