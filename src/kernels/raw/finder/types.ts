b1687c92b15764eae6233e5559814fed9e279ffe
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-30 04:12:45 +0300

    squash!

--- src/kernels/raw/finder/types.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-import { Uri } from 'vscode';
-
-export const ITrustedKernelPaths = Symbol('ITrustedKernelPaths');
-export interface ITrustedKernelPaths {
-    isTrusted(kernelPath: Uri): boolean;
-}
