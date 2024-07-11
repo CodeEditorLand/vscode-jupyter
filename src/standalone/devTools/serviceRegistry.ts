00ed5ade187c7b1b4afe520f215280fe7b02e2d7
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-12 01:41:51 +0300

    squash!

--- src/standalone/devTools/serviceRegistry.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-import { IExtensionContext } from '../../platform/common/types';
-import { addClearCacheCommand } from './clearCache';
-
-export function registerTypes(context: IExtensionContext, isDevMode: boolean) {
-    addClearCacheCommand(context, isDevMode);
-}
